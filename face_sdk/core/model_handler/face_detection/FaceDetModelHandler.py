d8880af80394575f31e08f8d08bedf01dc364de8
XJX777 <jixuanxu@163.com>
2021-01-26 17:22:06 +0800

    Update FaceDetModelHandler 0126

--- face_sdk/core/model_handler/face_detection/FaceDetModelHandler.py
+++ face_sdk/core/model_handler/face_detection/FaceDetModelHandler.py
@@ -120,9 +120,9 @@ class FaceDetModelHandler(BaseModelHandler):
         Return:
             decoded bounding box predictions
         """
-        boxes = torch.cat((
-            priors[:, :2] + loc[:, :2] * variances[0] * priors[:, 2:],
-            priors[:, 2:] * torch.exp(loc[:, 2:] * variances[1])), 1)
+        boxes = torch.cat((priors[:, :2], priors[:, 2:]), 1)
+        boxes[:, :2] = priors[:, :2] + loc[:, :2] * variances[0] * priors[:, 2:]
+        boxes[:, 2:] = priors[:, 2:] * torch.exp(loc[:, 2:] * variances[1])
         boxes[:, :2] -= boxes[:, 2:] / 2
         boxes[:, 2:] += boxes[:, :2]
         return boxes
