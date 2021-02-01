c0a5530939c3c25b0b0d929fc814b3fd4f702dd2
wangjun492 <wangjun492@jd.com>
2021-02-01 20:24:50 +0800

    correct cropped folder

--- test_protocol/utils/extractor/feature_extract_save.py
+++ test_protocol/utils/extractor/feature_extract_save.py
@@ -15,18 +15,18 @@ sys.path.append('/export/home/wangjun492/wj_armory/faceX-Zoo/face_recognition/ba
 from mobilefacenet_def import MobileFaceNet
 
 if __name__ == '__main__':
-    croped_face_folder = '/export2/wangjun492/face_database/public_data/final_data/test_data/megaface/face_crop_arcface'
+    cropped_face_folder = '/export2/wangjun492/face_database/public_data/final_data/test_data/megaface/face_crop_arcface'
     image_list_file = '/export2/wangjun492/face_database/public_data/final_data/test_data/megaface/face_crop_arcface/img_list.txt'
 
-    #croped_face_folder = '/export2/wangjun492/face_database/public_data/final_data/test_data/megaface/facescrub_mask_crop_arcface'
+    #cropped_face_folder = '/export2/wangjun492/face_database/public_data/final_data/test_data/megaface/facescrub_mask_crop_arcface'
     #image_list_file = '/export2/wangjun492/face_database/public_data/meta_data/test_data/megaface/mid_files/facescrub_img_list.txt'
 
     model_path = '/export/home/wangjun492/wj_armory/faceX-Zoo/face_recognition/trained_models/traditional_training/Mobilefacenets/Epoch_13.pt'
     feats_root = 'feat_dir'
 
     # extract feat by model.
-    #data_loader = DataLoader(CommonTestDataset_eye(croped_face_folder, image_list_file), batch_size=1024, num_workers=4, shuffle=False)
-    data_loader = DataLoader(CommonTestDataset(croped_face_folder, image_list_file), batch_size=10240, num_workers=4, shuffle=False)
+    #data_loader = DataLoader(CommonTestDataset_eye(cropped_face_folder, image_list_file), batch_size=1024, num_workers=4, shuffle=False)
+    data_loader = DataLoader(CommonTestDataset(cropped_face_folder, image_list_file), batch_size=10240, num_workers=4, shuffle=False)
     model = MobileFaceNet(512, 7, 7)
     model_dict = model.state_dict()
     pretrained_dict = torch.load(model_path)['state_dict']
